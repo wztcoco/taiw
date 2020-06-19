618467632614576fff1309e9b9f9afeb8e0e9fe7
wztcoco <1119172565@qq.com>
2020-06-19 13:00:28 +0800

    11

---
+++ src/main/java/com/colacoco/TaiwApplication.java
@@ -0,0 +1,13 @@
+package com.colacoco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaiwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaiwApplication.class, args);
+    }
+
+}
